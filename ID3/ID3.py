6dac99234d4dd40a320fbd5d8281ab09b7f95c70
Tal-Shab <tal.shab@campus.technion.ac.il>
2024-05-16 21:12:50 +0300

    return the >= and change the m choices array

--- ID3/ID3.py
+++ ID3/ID3.py
@@ -133,7 +133,7 @@ class ID3:
                 question = Question(self.label_names[curr_col], curr_col, threshold)
                 gain, true_rows, true_labels, false_rows, false_labels = self.partition(rows, labels, question, current_uncertainty)
                 #TODO: with >= we get 92.23 accuracy, with > we get 95.15
-                if gain > best_gain:
+                if gain >= best_gain:
                     best_gain = gain
                     best_question = question
                     best_true_rows = true_rows
--- ID3/ID3_experiments.py
+++ ID3/ID3_experiments.py
@@ -100,7 +100,7 @@ def cross_validation_experiment(plot_graph=True):
     num_folds = 5
 
     # ====== YOUR CODE: ======
-    m_choices = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    m_choices = [300, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
     assert len(m_choices) >= 5, 'fill the m_choices list with  at least 5 different values for M.'
 
@@ -165,14 +165,18 @@ if __name__ == '__main__':
            uncomment below code and run it
            modify the value from False to True to plot the experiment result
     """
-    plot_graphs = True
+
+    plot_graphs = False
     best_m = cross_validation_experiment(plot_graph=plot_graphs)
     print(f'best_m = {best_m}')
+
 
     """
         pruning experiment, run with the best parameter
         (*) To run the experiment uncomment below code and run it
     """
+    print("best_m_test start")
     acc = best_m_test(*data_split, min_for_pruning=best_m)
+    print("acc is ",acc)
     assert acc > 0.95, 'you should get an accuracy of at least 95% for the pruned ID3 decision tree'
     print(f'Test Accuracy: {acc * 100:.2f}%' if formatted_print else acc)
